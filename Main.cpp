ad52da85ff92dbb9cf300615dd8a1901e08fc887
Lawking0 <121373093+Lawking0@users.noreply.github.com>
2026-01-07 19:46:59 +0100

    figure out the transition of the timer

--- Main.cpp
+++ Main.cpp
@@ -7,15 +7,32 @@ int main(){
     using namespace std::chrono_literals;
 
     bool state=true; 
+    int counter=0;
 
-    while (state=false) {
-        if (state=true) {
+
+    while ( true) {
+        if (state) {
             std::cout<< "HIGH" <<std::endl;
-        } else {
+
+            std::this_thread::sleep_for(5s);
+
             std::cout<< "LOW" <<std::endl;
+
+            std::this_thread::sleep_for(5s);
+
+           counter++;
+
+           std::this_thread::sleep_for(5s);
+
+           std::cout<< "COUNTER: " << counter << std::endl;
+           
+           std::this_thread::sleep_for(5s);
+            if (counter>=5) {
+                state=false;
+            }
         }
 
-        std::this_thread::sleep_for(1s);
+        
 
         
     }
