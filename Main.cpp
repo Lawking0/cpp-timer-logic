f03619e703cb624e7120d42d8d426483fed8d648
Lawking0 <121373093+Lawking0@users.noreply.github.com>
2026-01-07 08:05:45 +0100

    final works the wait second. it runs

--- Main.cpp
+++ Main.cpp
@@ -1,18 +1,27 @@
 #include <iostream> 
+#include <thread>
+#include <chrono>
+
 
 int main(){
-  int first=1;
- std::string name= "HIGH";
- 
 
- while (first>=0) {
+    using namespace std::chrono_literals; 
 
-    std::string second="LOW";
- }
+    std::cout << "waiting 1 second..."<< std::endl;
 
+    std::this_thread::sleep_for(1s);
+
+
+ std::string first= "HIGH";
+ std::string second="LOW";
+
+ while (true) {
 std::cout<< first <<std::endl;
 
+std::cout << "Done waiting."<<std::endl;
 
-
+std::cout <<second <<std::endl;
+    
+ }
     return 0;
 }
